16bf23b9d054aae4899e52b792ca9eb9108236d5
Lukas Lehmann <lukas92lehmann@gmail.com>
2016-05-25 12:57:58 +0200

    change service desc

--- lib/app.rb
+++ lib/app.rb
@@ -9,7 +9,7 @@ You must bind a Redis service instance to this application.
 
 You can run the following commands to create an instance and bind to it:
 
-  $ cf create-service p-redis development redis-instance
+  $ cf create-service redis small redis-instance
   $ cf bind-service <app-name> redis-instance})
   end
 end
